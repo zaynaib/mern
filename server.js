5427f82a24c902d9409c300afbc4be1f61f85de5
Zaynaib <zaynaibg@gmail.com>
2017-11-18 20:32:19 -0600

    working mern nyt app

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact",
+  {
+    useMongoClient: true
+  }
+);
+
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});

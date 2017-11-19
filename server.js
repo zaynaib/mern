04f30d2e1076edcae058fa83f16aa1d2d766f6d3
Zaynaib <zaynaibg@gmail.com>
2017-11-18 21:11:53 -0600

    able to post new articles from postman

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -19,6 +23,30 @@ mongoose.connect(
     useMongoClient: true
   }
 );
+
+const db = require('./models');
+//console.log(db.Article);
+
+const {Article} = db
+
+// Article.create({
+// 	title: "insert",
+// 	url: "http://www.ola.com"
+// }).then(x => console.log(x))
+// .catch(x => console.log(x))
+
+app.post("/api/saved", (req,res) => {
+  var article = req.body
+  console.log(article)
+
+ Article.create(article)
+  .then(() => {
+    res.json(article)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
+})
 
 
 // Send every request to the React app
